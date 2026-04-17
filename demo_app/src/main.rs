88966d3dbc0b8d69ab89ce352e4cc5bfa41fa55c
Fumple <me@fumple.pl>
2026-04-17 16:46:55 +0200

    Added missing model impl block

--- demo_app/src/main.rs
+++ demo_app/src/main.rs
@@ -1,4 +1,6 @@
 mod models {
+    use std::fmt::Display;
+
     pub struct User {
         username: String,
         password: String,
@@ -13,6 +15,12 @@ mod models {
         }
         pub fn password(&self) -> &String {
             &self.password
+        }
+    }
+
+    impl Display for User {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_fmt(format_args!("User {}", self.username))
         }
     }
 }
