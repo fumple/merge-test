24fe0a13c60f5ee3c45c3f2caa09a8a8d087ec78
Fumple <me@fumple.pl>
2026-04-17 16:38:54 +0200

    Created User model

--- demo_app/src/main.rs
+++ demo_app/src/main.rs
@@ -1,3 +1,22 @@
+mod models {
+    pub struct User {
+        username: String,
+        password: String,
+    }
+
+    impl User {
+        pub fn new(username: String, password: String) -> Self {
+            User { username, password }
+        }
+        pub fn username(&self) -> &String {
+            &self.username
+        }
+        pub fn password(&self) -> &String {
+            &self.password
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
